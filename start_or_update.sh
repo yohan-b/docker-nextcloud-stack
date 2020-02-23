ebca3beba661437127b21d628c68c4aef075d009
yohan <783b8c87@scimetis.net>
2020-02-23 16:56:51 +0100

    Removed unnecessary output.

--- start_or_update.sh
+++ start_or_update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 for NETWORK in mailnet mysqlnet reverse-proxy
 do
-    sudo docker network inspect ${NETWORK} && continue
+    sudo docker network inspect ${NETWORK} &> /dev/null && continue
     sudo docker network create ${NETWORK}
-    sudo docker network inspect ${NETWORK} || \
+    sudo docker network inspect ${NETWORK} &> /dev/null || \
     { echo "ERROR: could not create network ${NETWORK}, exiting."; exit 1; }
 done
 
